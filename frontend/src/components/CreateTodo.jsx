557748e24ff2df649075e7278de700f9319882c1
DevDreamer <imbishalcool007@gmail.com>
2025-04-19 10:32:05 +0530

    dd

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,29 @@
+
+//in style we have gave two curlybrace
+//one is for styling and
+//because these are objects
+export function CreateTodo(){
+    return <div>
+        <input style={{
+            padding : 10,
+            margin : 10
+
+        }} type = "text" placeholder="title"></input> <br />
+
+        <input style={{
+            padding : 10,
+            margin : 10
+
+        }}
+        type = "text" placeholder="description"></input><br />
+        <button 
+        style={{
+            padding : 10,
+            margin : 10
+
+        }}
+        >Add a todo</button>
+    </div>
+}
+
+
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,7 @@
+export  function Todos(){
+    return <div>
+        <h1>Go to the Gym</h1>
+        <h2>You need to go to the gym</h2>
+        <button>Mark as complete</button>
+    </div>
+}
